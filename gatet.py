4cf9517c7176840fac33396fbfcb08c1a6786d21
gfemin <minthantshin.mts11@gmail.com>
2025-12-23 23:13:00 +0630

    Refactor Stripe payment method handling

--- gatet.py
+++ gatet.py
@@ -14,60 +14,58 @@ def Tele(ccx):
     random_amount1 = random.randint(1, 4)
     random_amount2 = random.randint(1, 99)
 
-	headers = {
-	    'authority': 'api.stripe.com',
-	    'accept': 'application/json',
-	    'accept-language': 'en-US,en;q=0.9',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://js.stripe.com',
-	    'referer': 'https://js.stripe.com/',
-	    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-site',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-	}
-	
-	data = f'type=card&billing_details[name]=Min+Thant&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=de40fe2a-ce5c-4324-b10a-87afe4df5c987a999e&muid=2436d5b2-8dcc-4b44-a486-96eecfae9ae1e7c877&sid=14287af9-bcf1-4c0b-bad9-195d30210bd788a022&payment_user_agent=stripe.js%2F78c7eece1c%3B+stripe-js-v3%2F78c7eece1c%3B+card-element&referrer=https%3A%2F%2Fchristiantvireland.ie&client_attribution_metadata[client_session_id]=22704580-3f18-4752-9028-705cbd3de38c&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51MrJUoFYWOfRAL36tEpAYV8qK1PEbiqp3QXs3wjZTLCImyIh2mmkYi8nW2tZBVvfgZG7UVaurtWfwkigqQAD6KJg00VB6fcBoS'
-	
-	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-	
-	pm = response.json()['id']
-	
-	
-	headers = {
-	    'Accept': 'application/json, text/javascript, */*; q=0.01',
-	    'Accept-Language': 'en-US,en;q=0.9',
-	    'Connection': 'keep-alive',
-	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'Origin': 'https://christiantvireland.ie',
-	    'Referer': 'https://christiantvireland.ie/donate/',
-	    'Sec-Fetch-Dest': 'empty',
-	    'Sec-Fetch-Mode': 'cors',
-	    'Sec-Fetch-Site': 'same-origin',
-	    'User-Agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-	    'X-Requested-With': 'XMLHttpRequest',
-	    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	}
-	
-	data = {
-	    'action': 'wp_full_stripe_inline_donation_charge',
-	    'wpfs-form-name': 'website_donation',
-	    'wpfs-form-get-parameters': '%7B%7D',
-	    'wpfs-custom-amount': 'other',
-	    'wpfs-custom-amount-unique': '1',
-	    'wpfs-donation-frequency': 'weekly',
-	    'wpfs-card-holder-email': 'minthantshin.virus11@gmail.com',
-	    'wpfs-card-holder-name': 'Min Thant',
-	    'wpfs-stripe-payment-method-id': f'{pm}',
-	}
-	
-	response = requests.post('https://christiantvireland.ie/wp-admin/admin-ajax.php', headers=headers, data=data)
+    headers = {
+        'authority': 'api.stripe.com',
+        'accept': 'application/json',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://js.stripe.com',
+        'referer': 'https://js.stripe.com/',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+    }
+    
+    data = f'type=card&billing_details[name]=Min+Thant&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=de40fe2a-ce5c-4324-b10a-87afe4df5c987a999e&muid=2436d5b2-8dcc-4b44-a486-96eecfae9ae1e7c877&sid=14287af9-bcf1-4c0b-bad9-195d30210bd788a022&payment_user_agent=stripe.js%2F78c7eece1c%3B+stripe-js-v3%2F78c7eece1c%3B+card-element&referrer=https%3A%2F%2Fchristiantvireland.ie&client_attribution_metadata[client_session_id]=22704580-3f18-4752-9028-705cbd3de38c&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51MrJUoFYWOfRAL36tEpAYV8qK1PEbiqp3QXs3wjZTLCImyIh2mmkYi8nW2tZBVvfgZG7UVaurtWfwkigqQAD6KJg00VB6fcBoS'
+    
+    response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+    
+    pm = response.json()['id']
+    
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://christiantvireland.ie',
+        'Referer': 'https://christiantvireland.ie/donate/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+    }
+    
+    data = {
+        'action': 'wp_full_stripe_inline_donation_charge',
+        'wpfs-form-name': 'website_donation',
+        'wpfs-form-get-parameters': '%7B%7D',
+        'wpfs-custom-amount': 'other',
+        'wpfs-custom-amount-unique': '1',
+        'wpfs-donation-frequency': 'weekly',
+        'wpfs-card-holder-email': 'minthantshin.virus11@gmail.com',
+        'wpfs-card-holder-name': 'Min Thant',
+        'wpfs-stripe-payment-method-id': f'{pm}',
+    }
+    
+    response = requests.post('https://christiantvireland.ie/wp-admin/admin-ajax.php', headers=headers, data=data)
     
     result = response.json()['message']
-    
     return result
